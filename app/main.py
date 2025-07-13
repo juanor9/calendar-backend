c96221f830f33cf0c275a596d5fe841fb22ec5b7
Juan Camilo Orjuela <15643199+juanor9@users.noreply.github.com>
2025-07-13 09:41:17 -0500

    task(VSC-29): create strawberry graphql

--- app/main.py
+++ app/main.py
@@ -1,17 +1,23 @@
 from fastapi import FastAPI
 from dotenv import load_dotenv
 from app.core.config import get_settings
+from strawberry.fastapi import GraphQLRouter
+from app.graphql.schema import schema
 
 load_dotenv()
 settings = get_settings()
 
+# instancia FastAPI
 app = FastAPI()
 
+# ✅ monta GraphQL en /graphql
+graphql_app = GraphQLRouter(schema)
+app.include_router(graphql_app, prefix="/graphql")
 
+# endpoint de salud
 @app.get("/health")
 async def health():
     return {
         "status": "ok",
         "database_url": settings.database_url
     }
-
